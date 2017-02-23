040b13787f3f46ef1c1ad65b5385d2a663906de7
Simon Elvery <simon@elvery.net>
2017-02-23 17:21:46 +1000

    Feature complete

--- src/index.js
+++ src/index.js
@@ -1,8 +1,51 @@
 const ns = require('util-news-selectors');
+const doc = document;
+const card = require('./card.hbs');
+const data = require('./data.json');
+const resultTemplate = require('./permanent.hbs');
+const container = doc.createElement('div');
+container.innerHTML = card(data);
+doc.getElementById('prc-init').appendChild(container);
 
-const storyEl = document.querySelector(ns('story'));
-const okEl = document.createElement('div');
+Array.prototype.slice.call(doc.querySelectorAll('[type="radio"][id^="prc-"]')).forEach(el => el.addEventListener('change', recalculate));
+Array.prototype.slice.call(doc.querySelectorAll('[type="text"][id^="prc-"]')).forEach(el => el.addEventListener('keyup', recalculate));
+doc.querySelector('#prc-container button').addEventListener('click', recalculate);
 
-okEl.className = 'interactive-penalty-rates-calculator-ok';
-okEl.innerHTML = '<pre>interactive-penalty-rates-calculator OK!</pre>';
-storyEl.appendChild(okEl);
+function recalculate() {
+	var industry;
+	var basis;
+	var rate;
+	var hours;
+
+	console.log(this);
+
+	try {
+		industry = doc.querySelector('[name="prc-industry"]:checked').value;
+		basis = doc.querySelector('[name="prc-employment-basis"]:checked').value;
+		rate = parseFloat(doc.getElementById('prc-hourly-rate').value.replace("$",''));
+		hours = parseFloat(doc.getElementById('prc-hours-on-sunday').value);
+	} catch (e) {}
+
+	if (!!industry && !!basis && !!rate && !!hours && rate > 10) {
+
+		doc.getElementById('prc-result').innerHTML = resultTemplate({
+			fastFood: industry === 'fast food',
+			sunday: (!("sunday" in data.industries[industry][basis])) ? false : {
+				loss: (hours * (data.industries[industry][basis].sunday[0] * rate - data.industries[industry][basis].sunday[1] * rate)).toFixed(2),
+				rateBefore: data.industries[industry][basis].sunday[0] * 100,
+				rateAfter: data.industries[industry][basis].sunday[1] * 100
+			},
+			holiday: (!("holiday" in data.industries[industry][basis])) ? false : {
+				loss: (hours * (data.industries[industry][basis].holiday[0] * rate - data.industries[industry][basis].holiday[1] * rate)).toFixed(2),
+				rateBefore: data.industries[industry][basis].holiday[0] * 100,
+				rateAfter: data.industries[industry][basis].holiday[1] * 100
+			}
+		});
+
+	} else {
+		if (this.className.indexOf('btn') > -1) {
+			doc.getElementById('prc-result').innerHTML = `<p class="error">Please complete all fields.</p>`;
+			return;
+		}
+	}
+}
